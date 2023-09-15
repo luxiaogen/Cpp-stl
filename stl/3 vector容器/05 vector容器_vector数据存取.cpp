b893942d635b91244c94a519bd79f362b281036f
luxiaogen <94947345+luxiaogen@users.noreply.github.com>
2023-09-15 14:10:33 +0800

    STL常用容器-vector容器

---
+++ stl/3 vector容器/05 vector容器_vector数据存取.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+// vector容器-数据存取
+
+/* void test01()
+{
+  vector<int> v;
+  for (int i = 0; i < 10; i++)
+    v.push_back(i + 1);
+  // 使用[]访问数组
+  for (int i = 0; i < v.size(); i ++)
+    cout << v[i] << " ";
+  cout << endl;
+  // 使用at访问数组
+  for (int i = 0;i < v.size(); i ++)
+    cout << v.at(i) << " ";
+  cout << endl;
+
+  // vector的第一个元素
+  cout << "首元素为:" << v.front() << endl;
+  // vector的尾元素
+  cout << "最后一个元素为：" << v.back() << endl;
+
+
+} */
+int main5()
+{
+  // test01();
+  return 0;
+}
---
+++ stl/3 vector容器/07 vector容器_vector预留空间.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*void test01()
+{
+  vector<int> v;
+  int num = 0; // 统计开辟次数
+  int *p = NULL;
+  // 利用reserve预留空间
+  v.reserve(100000);
+  for (int i = 0; i < 100000; i++)
+  {
+    v.push_back(i);
+    if (p != &v[0])
+    {
+      p = &v[0];
+      num++;
+    }
+  }
+  cout << "num = " << num << endl;
+}*/
+
+int main7()
+{
+  // test01();
+  return EXIT_SUCCESS;
+}
