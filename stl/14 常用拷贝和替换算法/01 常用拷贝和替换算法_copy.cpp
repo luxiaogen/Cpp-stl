948998f19eb698f4b58ed6f8faba062c4d1baaf3
luxiaogen <94947345+luxiaogen@users.noreply.github.com>
2023-09-16 23:20:10 +0800

    常用拷贝和替换算法

---
+++ stl/14 常用拷贝和替换算法/01 常用拷贝和替换算法_copy.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+// 常用拷贝和替换算法 copy
+/*void myPrint(int val)
+{
+  cout << val << " ";
+}
+void test01()
+{
+  vector<int> v1;
+  for (int i = 0; i < 10; i++)
+    v1.push_back(i + 1);
+  vector<int> v2;
+  // 提前分配空间，不然会报错
+  v2.resize(v1.size());
+  copy(v1.begin(), v1.end(), v2.begin());
+
+  for_each(v2.begin(), v2.end(), myPrint);
+  cout << endl;
+}*/
+int main1()
+{
+  // test01();
+  return 0;
+}
